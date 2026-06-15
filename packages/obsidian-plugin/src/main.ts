dccc416aba60197b0deb737d8a21dfc79b1398af
Kirill Kucherenkov <kirill.kucherenkov@gmail.com>
2026-06-15 11:01:02 +0400

    feat(obsidian-plugin): settings tab (obsidian-plugin)

--- packages/obsidian-plugin/src/main.ts
+++ packages/obsidian-plugin/src/main.ts
@@ -1,7 +1,19 @@
 import { Plugin } from 'obsidian';
+import { type BurkmakSettings, BurkmakSettingTab, DEFAULT_SETTINGS } from './settings';
 
 export default class BurkmakPlugin extends Plugin {
+  override settings: BurkmakSettings = { ...DEFAULT_SETTINGS };
+
   override async onload(): Promise<void> {
-    // Implementation in Task 5
+    await this.loadSettings();
+    this.addSettingTab(new BurkmakSettingTab(this.app, this));
+  }
+
+  async loadSettings(): Promise<void> {
+    this.settings = Object.assign({}, DEFAULT_SETTINGS, (await this.loadData()) as Partial<BurkmakSettings>);
+  }
+
+  async saveSettings(): Promise<void> {
+    await this.saveData(this.settings);
   }
 }
